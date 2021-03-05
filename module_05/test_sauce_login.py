50b4314080f173bbadf926b34d24a2e41012bc86
Luis Rivas <luis.rivas.0606@gmail.com>
2021-03-04 19:47:27 -0600

    Add inventory

--- module_05/test_sauce_login.py
+++ module_05/test_sauce_login.py
@@ -1,6 +1,7 @@
 """Test cases for login"""
 from selenium.webdriver.support.wait import WebDriverWait
 from common.webdriver_factory import get_driver
+from module_05.sauce_func_lib.inventory import get_inventory, InventoryItem
 from module_05.sauce_func_lib.login import login
 
 
@@ -9,4 +10,7 @@ def test_valid_user():
     wait = WebDriverWait(driver, 5)
     driver.get('https://www.saucedemo.com/')
     login(wait, 'standard_user', 'secret_sauce')
+    items = get_inventory(wait)
+    assert len(items) > 0, 'Inventory should be loaded'
+
 
