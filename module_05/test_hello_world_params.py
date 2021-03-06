9b755c4f60a39172b9cd687fbd000ab48df3c77e
Luis Rivas <luis.rivas.0606@gmail.com>
2021-03-06 12:12:24 -0600

    Add latest examples

--- module_05/test_hello_world_params.py
+++ module_05/test_hello_world_params.py
@@ -3,14 +3,14 @@ import pytest
 
 
 DATA = [
-    ('Tester1', 'QAMinds1'),
-    ('Tester2', 'QAMinds2'),
-    ('Tester3', 'QAMinds3'),
+    ('Tester1', 'pass1'),
+    ('Tester2', 'pass2'),
+    ('Tester3', 'pass3'),
 ]
 
 
-@pytest.mark.parametrize('first_name, last_name', DATA)
-def test_one(first_name, last_name):
+@pytest.mark.parametrize('user, password', DATA)
+def test_one(user, password):
     """Test case demo 1."""
-    print(f'{first_name}-{last_name}')
+    print(f'{user}-{password}')
 
--- module_05/test_sauce_login.py
+++ module_05/test_sauce_login.py
@@ -1,16 +1,37 @@
 """Test cases for login"""
+import pytest
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from common.webdriver_factory import get_driver
-from module_05.sauce_func_lib.inventory import get_inventory, InventoryItem
+from module_05.sauce_func_lib.inventory import get_inventory
 from module_05.sauce_func_lib.login import login
 
 
-def test_valid_user():
+LOGIN_DATA = [
+    ('standard_user', 'secret_sauce'),
+    ('performance_glitch_user', 'secret_sauce'),
+    ('problem_user', 'secret_sauce'),
+]
+
+
+@pytest.mark.parametrize('user, password', LOGIN_DATA)
+def test_valid_user(user: str, password: str):
     driver = get_driver('chrome')
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 10)
     driver.get('https://www.saucedemo.com/')
     login(wait, 'standard_user', 'secret_sauce')
     items = get_inventory(wait)
     assert len(items) > 0, 'Inventory should be loaded'
+    driver.close()
+
+
+def test_invalid_user():
+    driver = get_driver('chrome')
+    wait = WebDriverWait(driver, 5)
+    driver.get('https://www.saucedemo.com/')
+    login(wait, 'standard_user', 'invalid_secret')
+    with pytest.raises(TimeoutException):
+        get_inventory(wait)
+    driver.close()
 
 
